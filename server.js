d2acb801a632dfd4b8c2d20ac53d331e9819c14f
Emre Cakmak <emrcakmak@Emre-Mac-mini.local>
2023-03-03 00:54:55 +0300

    done with ssr

--- server.js
+++ server.js
@@ -2,23 +2,14 @@
 import express from 'express';
 import NodeFetch from 'node-fetch';
 import path from 'path';
-import manifest from './dist/client/ssr-manifest.json';
-import {render} from './dist/server/main.js';
-import {ssr} from './dist/server/package.json' assert { type: "json5" };
+import manifest from './dist/client/ssr-manifest.json' assert {type: "json"};
+import render from './dist/server/main.js';
+import ssr from './dist/server/package.json' assert {type: "json"};
+import { fileURLToPath } from 'url';
 
 
-//const express=require("express");
-//const NodeFetch=require("node-fetch").default;
-//const path=require("path");
-
-
-
-//const manifest=require("./dist/client/ssr-manifest.json");
-//const {default:render}=require("./dist/server/main.js");
-// const {
-//   ssr: {assets}
-// }=require('./dist/server/package.json')
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 
 
@@ -27,7 +18,7 @@ globalThis.fetch=NodeFetch;
 
 const app=express();
 
-for (const asset of ssr.assets) {
+for (const asset of ssr.ssr.assets) {
   app.use(
     '/'+asset,
     express.static(path.join(__dirname, 'dist/client/'+asset))
@@ -47,4 +38,6 @@ app.get('*', async (request,response)=>{
   response.end(html)
 })
 
-app.listen(4000)
+app.listen(4000,()=>{
+  console.log("Server is running")
+})
